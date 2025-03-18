cd1e75e17c08beec13f7a08da9353eff4838a863
Alistair Israel <aisrael@gmail.com>
2025-03-18 12:59:40 -0400

    Trying to fix tests

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
+import * as path from 'path'
 
 /**
  * The main function for the action.
@@ -10,17 +10,25 @@ export async function run(): Promise<void> {
   try {
     const service: string = core.getInput('service')
     const version: string = core.getInput('version')
+    const environment: string = core.getInput('environment')
     const pathToChart: string = core.getInput('path-to-chart')
     const pathToEnvVars: string = core.getInput('path-to-env-vars')
     const gitopsRepo: string = core.getInput('gitops-repo')
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Pushing ${service} version ${version} to ${gitopsRepo}`)
+    core.debug(`Pushing ${service} version ${version} to ${environment} in ${gitopsRepo}`)
+
+    const fullPathToChart: string = path.join(pathToChart, service)
+    const fullPathToEnvVars: string = path.join(fullPathToChart, pathToEnvVars)
+
+    core.debug(`Full path to chart: ${fullPathToChart}`)
+    core.debug(`Full path to env vars: ${fullPathToEnvVars}`)
 
     // TODO: Implement the actual logic for pushing the chart
+    // 1. checkout
 
     // Set outputs for other workflow steps to use
-    core.setOutput('commit-sha', 'TODO')
+    core.setOutput('commit-sha', '40365df9d45b56c6a73bd3dfdd442a6420ad4fba') // use a random string for now
     core.setOutput('commit-message', 'TODO')
   } catch (error) {
     // Fail the workflow run if an error occurs
